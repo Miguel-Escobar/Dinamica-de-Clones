dd2f93a778340bd09479bf9bd33e9b1723642ca3
Miguel <77211681+Miguel-Escobar@users.noreply.github.com>
2023-06-01 11:57:36 -0400

    Generación de imágenes. Trampa para ncrit

--- parameter_estimation.py
+++ parameter_estimation.py
@@ -11,10 +11,12 @@ def estimate_parameters(t, ndata, ccdfdata, initial_guess, bounds):
     params, cov = curve_fit(lambda n, birth_rate, death_rate, delta, n_crit: np.log(cdn.ccdfunc(n, [birth_rate, death_rate, delta, n_crit], t)), ndata, np.log(ccdfdata),
                             p0=initial_guess,
                             bounds=bounds,
-                            max_nfev=100,
-                            xtol=1e-8,
+                            max_nfev=10_000,
+                            xtol=1e-6,
+                            gtol=1e-7,
                             method='dogbox',
-                            verbose=2) # Lmao doesnt change parameters
+                            verbose=2,
+                            x_scale=[1, 1, 1, 10]) 
 
     return params, cov
 
@@ -30,27 +32,47 @@ if __name__ == '__main__':
     # Start code here
 
     datalocation = 'Data/20220222_idx.xlsm'
-    tcode = 7
-    initial_guess = [1/100., 1/1000., 9., 100.]
-
+    # initial_guess = [1/100., 1/1000., 9., 1.]
+    guesses = [[1/100., 1/1000., 9., 10.],
+               [1/100., 1/1000., 9., 10.],
+               [1/100., 1/1000., 9., 5.],
+               [1/100., 1/1000., 9., 4.],
+               [1/100., 1/1000., 9., 30.],
+               [1/100., 1/1000., 9., 17.],
+               [1/100., 1/1000., 9., 20.],
+               [1/100., 1/1000., 9., 50.]]
     
-    t = [24, 24, 3*24, 3*24, 6*24, 6*24, 13*24, 13*24][tcode-1]
+    tcodes = [1, 2, 3, 4, 5, 6, 7, 8]
 
-    data = an.read_excel_data(datalocation)
-    ndata, ccdfdata = an.ccdf_at_tcode(tcode, data)
-    bounds = ([0, 0, 0, 0], [10, 10, 10, 1000])  # Tuneable.
 
-    params, cov = estimate_parameters(t, ndata, ccdfdata, initial_guess, bounds)
+    with open('params.txt', 'w') as file:
+        for tcode in tcodes:
+            
+            t = [24, 24, 3*24, 3*24, 6*24, 6*24, 13*24, 13*24][tcode-1]
+            initial_guess = guesses[tcode-1]
 
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(111)
-    ax.plot(ndata, ccdfdata, 'o', label='Data')
-    ax.plot(ndata, cdn.ccdfunc(ndata, params, t), label='Fit')
-    ax.set_yscale('log')
-    ax.set_xlabel('Clone size')
-    ax.set_ylabel('CCDF')
-    ax.legend()
-    fig.show()
+            data = an.read_excel_data(datalocation)
+            ndata, ccdfdata = an.ccdf_at_tcode(tcode, data)
+            bounds = ([0, 0, 0, 0], [10, 10, 100, 5000])  # Tuneable.
+
+            params, cov = estimate_parameters(t, ndata, ccdfdata, initial_guess, bounds)
+
+            print(f"tcode: {tcode}, params: {params}")
+
+            file.write(f"tcode: {tcode}, params: {params} (birth_rate, death_rate, delta, n_crit)\n")
+
+            fig = plt.figure(figsize=(8, 6))
+            fig.clf()
+            ax = fig.add_subplot(111)
+            ax.plot(ndata, ccdfdata, 'o', label='Data')
+            ax.plot(ndata, cdn.ccdfunc(ndata, params, t), label='Fit')
+            ax.set_title(f"t: {t} [Hrs], EGF = {-1 + 2*(tcode % 2)}")
+            ax.set_yscale('log')
+            ax.set_xlabel('Clone size')
+            ax.set_ylabel('CCDF')
+            ax.legend()
+            fig.savefig(f"images/tcodefit_{tcode}.png", dpi=600)
+
 
     # End code here
 
