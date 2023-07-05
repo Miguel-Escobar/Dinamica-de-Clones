9ae963bdf45627c5e02f30a00b7fe53e97410283
Miguel <77211681+Miguel-Escobar@users.noreply.github.com>
2023-07-05 18:48:33 -0400

    Filled with determination

--- exp_sum_model.py
+++ exp_sum_model.py
@@ -6,12 +6,12 @@ from scipy.optimize import curve_fit
 # Note: this needs a more careful choice of initial guess than the current one.
 
 
-def biexponential_ccdf(n, rate_1, rate_2):
-    ccdf = .5*(np.exp(-rate_1*n) + np.exp(-rate_2*n))
+def biexponential_ccdf(n, rate_1, rate_2, convex_coeff):  # Hacer la combinación convexa de dos exponenciales.
+    ccdf = convex_coeff*np.exp(-rate_1*n) + (1-convex_coeff)*np.exp(-rate_2*n)
     return ccdf
 
-def log_biexponential_ccdf(n, rate_1, rate_2):
-    logccdf = np.log(.5*(np.exp(-rate_1*n) + np.exp(-rate_2*n)))
+def log_biexponential_ccdf(n, rate_1, rate_2, convex_coeff):
+    logccdf = np.log(convex_coeff*np.exp(-rate_1*n) + (1-convex_coeff)*np.exp(-rate_2*n))
     return logccdf
 
 def estimate_parameters(ndata, ccdfdata, initial_guess, bounds):
@@ -32,17 +32,17 @@ if __name__ == '__main__':
 
     # Initial guess for parameters.
 
-    initial_guess = [0.002, 0.001]
+    initial_guess = [0.002, 0.001, .5]
     tcodes = [1, 2, 3, 4, 5, 6, 7, 8]
 
     with open('expsum_params.csv', 'w') as file:
 
-        file.write(f"tcode, rate_1, rate_2, r_score\n")
+        file.write(f"tcode, rate_1, rate_2, convex_coefficient, r_score\n")
 
         for tcode in tcodes:
             data = an.read_excel_data(datalocation)
             ndata, ccdfdata = an.ccdf_at_tcode(tcode, data)
-            bounds = ([0, 0], [10, 10])  # Tuneable.
+            bounds = ([0, 0, 0], [10, 10, 1])  # Tuneable.
 
             print(f"\n Processing tcode {tcode}, with initial params {initial_guess}\n")
 
@@ -51,7 +51,7 @@ if __name__ == '__main__':
             print(f"\n Params at tcode {tcode}: {params}\n")
 
             r2 = an.r_score(log_biexponential_ccdf, params, ndata, np.log(ccdfdata))
-            file.write(f"{tcode}, {params[0]}, {params[1]}, {r2}\n")
+            file.write(f"{tcode}, {params[0]}, {params[1]}, {params[2]}, {r2}\n")
 
             t = [24, 24, 3*24, 3*24, 6*24, 6*24, 13*24, 13*24][tcode-1]
 
@@ -59,7 +59,7 @@ if __name__ == '__main__':
             fig.clf()
             ax = fig.add_subplot(111)
             ax.plot(ndata, ccdfdata, 'o', label='Data')
-            ax.plot(ndata, biexponential_ccdf(ndata, params[0], params[1]), label='Fit')
+            ax.plot(ndata, biexponential_ccdf(ndata, params[0], params[1], params[2]), label='Fit')
             ax.set_title(f"t: {t} [Hrs], EGF = {-1 + 2*(tcode % 2)}")
             ax.set_yscale('log')
             ax.set_xlabel('Clone size')
